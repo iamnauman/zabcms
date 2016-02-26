e6c92828df4855fb2a92b73dff4dfc9b0ab1c879
Faizan Aamer <faizanaryan94@gmail.com>
2016-02-27 01:17:52 +0500

    dbcon to Dbcon------------------Loginservlet----------------Regservlet------------------Userdao---------------Userdto

---
+++ Zabcms/src/com/dal/Loginservlet.java
@@ -0,0 +1,30 @@
+package com.dal;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Loginservlet")
+public class Loginservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public Loginservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
