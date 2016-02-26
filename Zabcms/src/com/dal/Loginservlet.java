e19106c7f7edf9a7438cef86b18d410bbc709cb3
Faizan Aamer <faizanaryan94@gmail.com>
2016-02-27 04:01:03 +0500

    login servlet completed

--- Zabcms/src/com/dal/Loginservlet.java
+++ Zabcms/src/com/dal/Loginservlet.java
@@ -1,30 +1,58 @@
 package com.dal;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-
+import com.model.Userdto;
+import com.dal.Userdao;
 @WebServlet("/Loginservlet")
 public class Loginservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+   RequestDispatcher dis;
    
-    public Loginservlet() {
+  /* private Loginservlet() {
         super();
-        // TODO Auto-generated constructor stub
-    }
+       
+    }*/
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+	
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username,password;
+		username=request.getParameter("username");
+		password=request.getParameter("password");
+		Userdao uDao = new Userdao(getServletContext());
+		Userdto um = new Userdto();
+		um.setUsername(username);
+		um.setPassword(password);
+		System.out.println("Login Servlet:"+um.getUsername());
+		if(uDao.checkUser(um)){
+			System.out.println("Logged In");
+			HttpSession session = request.getSession();
+			session.setAttribute("username", username);
+			System.out.println("Dispatching");
+			dis = request.getRequestDispatcher("HistoryServlet?action=showHistory");
+			dis.forward(request, response);
+			System.out.println("Done");
+		}
+		else{
+			System.out.println("Error");
+			dis = request.getRequestDispatcher("error.html");
+			dis.forward(request, response);
+		}
 	}
 
-}
+
+
+	}
+
+
